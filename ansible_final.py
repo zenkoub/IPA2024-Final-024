e1bd6904e7654ccc1bcb04002c0ff96f01a2e2bd
Chotipat P. <chotipat@it.kmitl.ac.th>
2024-10-24 10:55:35 +0700

    Update ansible_final.py

--- ansible_final.py
+++ ansible_final.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def backup():
+def showrun():
     # read https://www.datacamp.com/tutorial/python-subprocess to learn more about subprocess
     command = ['<!!!REPLACEME with ansible command to run playbook!!!>', '<!!!REPLACEME with playbook yaml file!!!>']
     result = subprocess.run(command, capture_output=True, text=True)
