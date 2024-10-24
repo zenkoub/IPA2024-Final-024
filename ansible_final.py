fa5dff0227d33b8f84feb3b4853b5161d03b2263
Chotipat P. <chotipat@it.kmitl.ac.th>
2024-10-24 10:54:13 +0700

    Update ansible_final.py

--- ansible_final.py
+++ ansible_final.py
@@ -5,7 +5,6 @@ def backup():
     command = ['<!!!REPLACEME with ansible command to run playbook!!!>', '<!!!REPLACEME with playbook yaml file!!!>']
     result = subprocess.run(command, capture_output=True, text=True)
     result = result.stdout
-    # all two tasks in playbook are ok, return 'ok', if not return 'Error: Ansible'
     if 'ok=2' in result:
         return <!!!REPLACEME!!!>
     else:
